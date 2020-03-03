f9056d28682b221b6ea8e4f38480a9a0ac34bf72
Håkan Åkerberg <hakan@tarra.se>
2020-03-03 21:17:32 +0100

    0.1.7.b1

---
+++ custom_components/shelly/block.py
@@ -0,0 +1,111 @@
+"""
+Shelly block.
+
+For more details about this platform, please refer to the documentation
+https://home-assistant.io/components/shelly/
+"""
+
+from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.util import slugify #, dt as dt_util
+from homeassistant.const import CONF_NAME
+
+from .const import (CONF_OBJECT_ID_PREFIX, CONF_ENTITY_ID,
+                    CONF_SHOW_ID_IN_NAME, DOMAIN)
+
+class ShellyBlock(RestoreEntity):
+    """Base class for Shelly entities"""
+
+    def __init__(self, block, instance, prefix=""):
+        conf = instance.conf
+        id_prefix = conf.get(CONF_OBJECT_ID_PREFIX)
+        self._unique_id = slugify(id_prefix + "_" + block.type + "_" +
+                                  block.id + prefix)
+        self.entity_id = "." + self._unique_id
+        entity_id = \
+            instance._get_specific_config(CONF_ENTITY_ID, None, block.id)
+        if entity_id is not None:
+            self.entity_id = "." + slugify(id_prefix + "_" + entity_id + prefix)
+            self._unique_id += "_" + slugify(entity_id)
+        self._show_id_in_name = conf.get(CONF_SHOW_ID_IN_NAME)
+        self._block = block
+        self.hass = instance.hass
+        self.instance = instance
+        self._block.cb_updated.append(self._updated)
+        block.shelly_device = self  #todo, should be array??
+        self._name = instance._get_specific_config(CONF_NAME, None, block.id)
+        self._name_ext = None
+        self._is_removed = False
+        self.async_on_remove(self._remove_handler)
+
+    def _remove_handler(self):
+        self._is_removed = True
+        self._block.cb_updated.remove(self._updated)
+
+    @property
+    def name(self):
+        """Return the display name of this device."""
+        if self._name is None:
+            name = self._block.friendly_name()
+        else:
+            name = self._name
+        if self._name_ext:
+            name += ' - ' + self._name_ext
+        if self._show_id_in_name:
+            name += " [" + self._block.id + "]"
+        return name
+
+    def _updated(self, _block):
+        """Receive events when the switch state changed (by mobile,
+        switch etc)"""
+        disabled = self.registry_entry and self.registry_entry.disabled_by
+        if self.entity_id is not None and not self._is_removed \
+            and not disabled:
+            self.schedule_update_ha_state(True)
+
+    @property
+    def device_state_attributes(self):
+        """Show state attributes in HASS"""
+        attrs = {'ip_address': self._block.ip_addr,
+                 'shelly_type': self._block.type_name(),
+                 'shelly_id': self._block.id,
+                 'discovery': self._block.discovery_src
+                }
+
+        room = self._block.room_name()
+        if room:
+            attrs['room'] = room
+
+        if self._block.info_values is not None:
+            for key, value in self._block.info_values.items():
+                if self.instance.conf_attribute(key):
+                    attrs[key] = value
+
+        return attrs
+
+    @property
+    def device_info(self):
+        return {
+            'identifiers': {
+                (DOMAIN, self._block.unit_id)
+            },
+            'name': self._block.friendly_name(),
+            'manufacturer': 'Allterco',
+            'model': self._block.type_name(),
+            'sw_version': self._block.fw_version()
+        }
+
+        return self.instance.build_device_info(self._block)
+
+    @property
+    def unique_id(self):
+        """Return the ID of this device."""
+        return self._unique_id
+
+    def remove(self):
+        self._is_removed = True
+        self.hass.add_job(self.async_remove)
+
+    @property
+    def available(self):
+        """Return true if switch is available."""
+        return self._block.available()
---
+++ custom_components/shelly/device.py
@@ -0,0 +1,138 @@
+"""
+Shelly device.
+
+For more details about this platform, please refer to the documentation
+https://home-assistant.io/components/shelly/
+"""
+
+from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.util import slugify
+from homeassistant.const import CONF_NAME
+
+from .const import (CONF_OBJECT_ID_PREFIX, CONF_ENTITY_ID, CONF_SHOW_ID_IN_NAME,
+                    ALL_SENSORS, SENSOR_TYPES_CFG, DOMAIN)
+
+class ShellyDevice(RestoreEntity):
+    """Base class for Shelly entities"""
+
+    def __init__(self, dev, instance):
+        conf = instance.conf
+        id_prefix = conf.get(CONF_OBJECT_ID_PREFIX)
+        self._unique_id = id_prefix + "_" + dev.type + "_" + dev.id
+        self.entity_id = "." + slugify(self._unique_id)
+        entity_id = instance._get_specific_config(CONF_ENTITY_ID,
+                                         None, dev.id, dev.block.id)
+        if entity_id is not None:
+            self.entity_id = "." + slugify(id_prefix + "_" + entity_id)
+            self._unique_id += "_" + slugify(entity_id)
+        self._show_id_in_name = conf.get(CONF_SHOW_ID_IN_NAME)
+        self._name_ext = None
+        #self._name = dev.type_name()
+        #if conf.get(CONF_SHOW_ID_IN_NAME):
+        #    self._name += " [" + dev.id + "]"  # 'Test' #light.name
+        self._dev = dev
+        self.hass = instance.hass
+        self.instance = instance
+        self._dev.cb_updated.append(self._updated)
+        dev.shelly_device = self
+        self._name = instance._get_specific_config(CONF_NAME, None,
+                                          dev.id, dev.block.id)
+
+        self._sensor_conf = instance._get_sensor_config(dev.id, dev.block.id)
+
+        self._is_removed = False
+
+    def _updated(self, _block):
+        """Receive events when the switch state changed (by mobile,
+        switch etc)"""
+        if self.entity_id is not None and not self._is_removed:
+            self.schedule_update_ha_state(True)
+
+        if self._dev.info_values is not None:
+            device_sensors = self.instance.device_sensors
+            for key, _value in self._dev.info_values.items():
+                ukey = self._dev.id + '-' + key
+                if not ukey in device_sensors:
+                    device_sensors.append(ukey)
+                    for sensor in self._sensor_conf:
+                        if ALL_SENSORS[sensor].get('attr') == key:
+                            attr = {'sensor_type':key,
+                                    'itm':self._dev}
+                            if key in SENSOR_TYPES_CFG and \
+                                SENSOR_TYPES_CFG[key][4] == 'bool':
+                                self.instance.add_device("binary_sensor", attr)
+                            else:
+                                self.instance.add_device("sensor", attr)
+
+    @property
+    def name(self):
+        """Return the display name of this device."""
+        if self._name is None:
+            name = self._dev.friendly_name()
+        else:
+            name = self._name
+        if self._name_ext:
+            name += ' - ' + self._name_ext
+        if self._show_id_in_name:
+            name += " [" + self._dev.id + "]"
+        return name
+
+    @property
+    def device_state_attributes(self):
+        """Show state attributes in HASS"""
+        attrs = {'ip_address': self._dev.ip_addr,
+                 'shelly_type': self._dev.type_name(),
+                 'shelly_id': self._dev.id,
+                 'discovery': self._dev.discovery_src
+                }
+        room = self._dev.room_name()
+        if room:
+            attrs['room'] = room
+
+        if self._dev.block.info_values is not None:
+            for key, value in self._dev.block.info_values.items():
+                if self.instance.conf_attribute(key):
+                    attrs[key] = value
+
+        if self._dev.info_values is not None:
+            for key, value in self._dev.info_values.items():
+                if self.instance.conf_attribute(key):
+                    attrs[key] = value
+
+        if self._dev.sensor_values is not None:
+            for key, value in self._dev.sensor_values.items():
+                if self.instance.conf_attribute(key):
+                    attrs[key] = value
+
+        return attrs
+
+    @property
+    def device_info(self):
+        return {
+            'identifiers': {
+                (DOMAIN, self._dev.block.id)
+            },
+            'name': self._dev.block.friendly_name(),
+            'manufacturer': 'Allterco',
+            'model': self._dev.type_name(),
+            'sw_version': self._dev.fw_version()
+        }
+
+    @property
+    def unique_id(self):
+        """Return the ID of this device."""
+        return self._unique_id
+
+    @property
+    def available(self):
+        """Return true if switch is available."""
+        return self._dev.available()
+
+    def remove(self):
+        self._is_removed = True
+        self.hass.add_job(self.async_remove)
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return False
