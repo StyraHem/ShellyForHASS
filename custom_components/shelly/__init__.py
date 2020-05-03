746c440e02aa6ed3bbe193facdc31955da720e87
Håkan Åkerberg <hakan@tarra.se>
2020-05-03 17:13:49 +0200

    0.1.8

--- custom_components/shelly/__init__.py
+++ custom_components/shelly/__init__.py
@@ -32,7 +32,7 @@ from .configuration_schema import CONFIG_SCHEMA, CONFIG_SCHEMA_ROOT
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = "0.1.8-b2"
+__version__ = "0.1.8"
 VERSION = __version__
 
 async def async_setup(hass, config):
