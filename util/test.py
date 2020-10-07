5f8cebc926383aa093e26a6289e3163a0a17855c
Håkan Åkerberg <hakan@tarra.se>
2020-10-07 16:43:59 +0200

    Update test.py

--- util/test.py
+++ util/test.py
@@ -1,17 +1,18 @@
 import socket
 import struct
 
-#You can test to add your ip address here (host_ip)
-MY_IP = socket.INADDR_ANY
-#MY_IP = socket.inet_aton("192.168.x.x")
-
 UDP_IP = "224.0.1.187"
 UDP_PORT = 5683
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', UDP_PORT))
-mreq = struct.pack("=4sl", socket.inet_aton(UDP_IP), MY_IP)
+
+mreq = struct.pack("=4sl", socket.inet_aton(UDP_IP), socket.INADDR_ANY)
+
+#You can test to add your ip address here (host_ip in config)
+#mreq = struct.pack("=4s4s", socket.inet_aton(UDP_IP), socket.inet_aton("192.168.x.x"))
+
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
