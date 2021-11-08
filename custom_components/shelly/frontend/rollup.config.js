692b979c4915cbfb177f3931abbb929c38503138
Håkan Åkerberg <hakan@tarra.se>
2021-11-09 00:07:33 +0100

    Add support for Config GUI, Shelly Plus 1/1P and Shelly Pro 4PM using MQTT or Http/Socket

--- custom_components/shelly/frontend/rollup.config.js
+++ custom_components/shelly/frontend/rollup.config.js
@@ -5,7 +5,6 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import typescript from "rollup-plugin-typescript2";
-import postcss from 'rollup-plugin-postcss'
 import { visualizer } from 'rollup-plugin-visualizer';
 import styles from "rollup-plugin-styles";
 
